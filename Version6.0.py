e3e83ede2471bca0ab1e67d29adc10f9c59048dc
CharlesFrss <128299071+CharlesFrss@users.noreply.github.com>
2023-03-21 16:08:04 +0100

    Update Version6.0.py

--- Version6.0.py
+++ Version6.0.py
@@ -2,12 +2,13 @@
 
 import os
 import sys
-
+import subprocess
 # Installation des dépendances nécessaires
 os.system("sudo apt-get update")
 os.system("sudo apt-get install nmap")
 os.system("sudo apt-get install python3-pip")
-pip install -r requirements.txt
+subprocess.call(['pip3', 'install', '-r', 'requirements.txt'])
+#pip install -r requirements.txt
 
 import nmap
 from googlesearch import search
